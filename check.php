b7317df68fd98e3d6a33452e582bd89e4d88e679
no_riko <noriko0521j@gmail.com>
2019-08-28 15:53:35 +0800

    追記

---
+++ check.php
@@ -0,0 +1,47 @@
+<?php
+    //スーパーグローバル変数
+    // -
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $content = $_POST["content"];
+
+    if($username == " "){
+        $usernameResult = "ユーザー名が入力されていません";
+    }else{
+        $usernameResult = $username;
+    }
+    if($email == " "){
+        $emailResult = "ユーザー名が入力されていません";
+    }else{
+        $emailResult = $email;
+    }
+    if($content == " "){
+        $contentResult = "ユーザー名が入力されていません";
+    }else{
+        $contentResult = $content;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>入力内容確認</title>
+</head>
+<body>
+
+<h1>入力内容確認</h1>
+
+<p>名前：<?php echo $_POST["username"]; ?></p>
+<p>メールアドレス：<?php echo $_POST["email"]; ?></p>
+<p>内容：<?php echo $_POST['content']; ?></p>
+
+<form action="">
+    <button>戻る</button>
+    <input type="text" value="OK">
+</form>
+    
+</body>
+</html>
